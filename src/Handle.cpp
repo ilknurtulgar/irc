3d72071265913baabee683d72a2e56a8e0afb76e
Zerrin Ayaz <zayaz@k1m22s04.42istanbul.com.tr>
2025-09-13 17:45:10 +0300

    added user func

--- src/Handle.cpp
+++ src/Handle.cpp
@@ -6,7 +6,7 @@
 /*   By: zayaz <zayaz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 12:59:26 by itulgar           #+#    #+#             */
-/*   Updated: 2025/09/13 16:24:39 by zayaz            ###   ########.fr       */
+/*   Updated: 2025/09/13 17:43:05 by zayaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,11 @@ void Client::handlePass(std::vector<std::string> data){
 	
 }	
 
-void Client::handleUser(std::vector<std::string> data){
-	
-(void)data;
-}
-
 bool isValidNickname(const std::string& nickName) {
     if (nickName.empty() || !std::isalpha(nickName[0]))
         return false;
 
-    for (int i = 0; i < nickName.size(); i++) {
+    for (size_t i = 0; i < nickName.size(); i++) {
         char c = nickName[i];
         if (!std::isalnum(c) && c != '-' && c != '_' && c != '[' && c != ']' &&
             c != '{' && c != '}' && c != '\\' && c != '`' && c != '^') {
@@ -63,3 +58,33 @@ void Client::handleNick(std::vector<std::string> data){
     nickName = data[1];
     std::cout << "Nickname set to: " << nickName << std::endl;
 }
+
+// USER <username> <unused> <unused> :zerrin ayaz //burada real nama arasında boslık olbialir
+
+void Client::handleUser(std::vector<std::string> data){
+	
+	std::cout << "size: " << data.size() << std::endl;
+
+	if (data.size() < 5 || data[4].size() < 2 || data[4][0] != ':') {
+			std::string errorMsg = ":irc.server.com 461 * USER :Not enough parameters\r\n";
+			send(clientSocketFd, errorMsg.c_str(), errorMsg.length(), 0);
+			return;
+	}
+	userName = data[1];
+    hostName = data[2];
+    serverName = data[3];
+	
+	if(data.size()> 5)
+	{
+		realName = data[4].substr(1);
+		for (size_t i = 5; i < data.size(); ++i) {
+			realName += " " + data[i];
+		}
+	} else
+		realName = data[4].substr(1);
+	
+	std::cout << "User info set - username: " << userName << ", realname: " << realName << std::endl;
+	std::string welcomeMsg = ":irc.server.com 001 " + nickName + " :Welcome to the IRC server\r\n";
+    send(clientSocketFd, welcomeMsg.c_str(), welcomeMsg.length(), 0);
+}
+
